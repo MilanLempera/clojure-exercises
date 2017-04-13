2f6267fe278807ec3f0399ce6cc02d4ff8030f49
Milan Lempera <milanlempera@gmail.com>
2017-04-13 11:47:47 +0200

    add queue and set

--- src/clojure_exercises/basics/collections.clj
+++ src/clojure_exercises/basics/collections.clj
@@ -1,4 +1,5 @@
-(ns clojure-exercises.basics.collections)
+(ns clojure-exercises.basics.collections
+  (:import (clojure.lang PersistentQueue)))
 
 ; vectors
 
@@ -35,14 +36,46 @@
 
 ; lists
 
+(cons 0 '(1 2))
+(conj '(1 2) 0)
+
 (peek '(1 2 3 4 5))
+(first '(1 2 3 4 5))
+
 (pop '(1 2 3 4 5))
+(next '(1 2 3 4 5))
 
 (list? 'a)
+
+; persistent queue
+; does not have print
+
+(defmethod print-method clojure.lang.PersistentQueue
+  [q w]
+
+  (print-method '<- w)
+  (print-method (seq q) w)
+  (print-method '-< w))
+
+(def steps (conj clojure.lang.PersistentQueue/EMPTY :first-step :second-step :third-step))
+(peek steps)
+(pop steps)
+(rest steps)                                                ; returns sequnce not PersistentQueue
+
+; persistent set
+
+(#{:a :b :c} :b)
+(#{:a :b :c} :f)
+
+(get #{:a :b :c} :a)
+(get #{:a :b :c} :f )
+
+
+
+
+
 
 (get [:a :b :c :d] 2)
 (nth [:a :b :c :d] 3)
 
-(peek [:a :b :c :d])
 (rseq [:a :b :c :d])
-(vector? [:a :b :c])
